815a2248e9b900483fe88c1558c27edbd613c6d4
William Barshop <wbarshop@ucla.edu>
2019-06-08 16:48:53 -0700

    One up

--- tag_grab.sh
+++ tag_grab.sh
@@ -1,8 +1,8 @@
 ##### This script is responsible for some basic staging of necessary files for
 ##### both Kaiko (and its default network) and TagGraph.
-git clone https://github.com/PNNL-Comp-Mass-Spec/Kaiko.git
-cd Kaiko
-build_docker.sh
-cd model
-get_data.sh
+git clone https://github.com/PNNL-Comp-Mass-Spec/Kaiko.git && \
+cd Kaiko && \
+sudo sh ${PWD}/build_docker.sh && \
+cd model && \
+sh get_data.sh
 
