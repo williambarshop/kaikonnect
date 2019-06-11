92e7f0286cdf81cfc12d3932aea102520ab9fe71
William Barshop <william.barshop@gmail.com>
2019-06-10 17:38:32 -0700

    More bug fixes and functionality updates...

--- kaikonnect.py
+++ kaikonnect.py
@@ -1,18 +1,21 @@
-import os, shutil, glob, sys, subprocess, importlib
+import os, shutil, glob, sys, subprocess, importlib, time
 
 #From stackoverflow #12332975
 def install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", package])
 
-try:
-    import optparse
-    package_list=["optparse"]
-except:
-    #Let's make sure we have the packages we need here to start...
-    for each_package in package_list:
+package_list=["optparse"]  #Packages users may be unlikely to have....
+
+for each_package in package_list:
+    try:
+        #import optparse
+        globals()[each_package]=importlib.import_module(each_package)
+    except Exception as e:
+        print e
+        #Let's make sure we have the packages we need here to start...
         print "Making sure that package \"{0}\" is installed...".format(each_package)
         install(each_package)
-        importlib.import_module(each_package)
+        globals()[each_package]=importlib.import_module(each_package)
 
 
 
@@ -31,11 +34,29 @@ parser.add_option("--kaiko_beam_size",action="store",type="int",dest="kaiko_beam
 
 
 #TAG_GRAPH OPTIONS
+parser.add_option("--tg_per_fraction",action="store_false",dest="tg_outputPerFraction",default=False)
+parser.add_option("--tg_FDR_cutoff",action="store",type="float",dest="tg_FDRCutoff",default=0.01)
+
+parser.add_option("--tg_logEM_cutoff",action="store",type="int",dest="tg_logEMCutoff",default=2)
+parser.add_option("--tg_Display_Protein_Num",action="store",type="int",dest="tg_DisplayProteinNum",default=3)
+parser.add_option("--tg_ExperimentName",action="store",type="string",dest="tg_ExperimentName",default="Expt_{0}".format(time.strftime("%Y-%m-%d_%H-%M")))
 
 
 
 
 (options,args) = parser.parse_args()
+
+
+
+def makeDirCheck(dir_to_make): #Helper function for making directories if they don't exist....
+    if not os.path.isdir(dir_to_make):
+        try:
+            os.mkdir(dir_to_make)
+        except:
+            print "\n\nERROR: Failed to make directory {0} !\nPlease ensure that you have rights to make this directory, or that it isn\'t a file already.".format(dir_to_make)
+
+
+
 
 #Let's establish the starting directory...
 starting_dir=os.getcwd()
@@ -53,11 +74,12 @@ if options.mzml_folder is None:
     print "\n\nERROR: You'll need to supply an mzML folder location via \"--mzML_folder {value}\""
     sys.exit(2)
 else:
-    if not os.path.isdir(options.mzml_folder):
-        try:
-            os.mkdir(options.mzml_folder)
-        except:
-            print "\n\nERROR: Failed to make directory {0} !\nPlease ensure that you have rights to make this directory, or that it isn\'t a file already.".format(options.mzml_folder)
+    makeDirCheck(options.mzml_folder)
+#    if not os.path.isdir(options.mzml_folder):
+#        try:
+#            os.mkdir(options.mzml_folder)
+#        except:
+#            print "\n\nERROR: Failed to make directory {0} !\nPlease ensure that you have rights to make this directory, or that it isn\'t a file already.".format(options.mzml_folder)
 
 os.chdir(options.mzml_folder)
 full_mzml_dir=os.getcwd() #For later reference...
@@ -69,11 +91,12 @@ if options.output_folder is None:
     print "\n\nERROR: You'll need to supply an output folder location via \"--output_folder {value}\""
     sys.exit(2)
 else:
-    if not os.path.isdir(options.output_folder):
-        try:
-            os.mkdir(options.output_folder)
-        except:
-            print "\n\nERROR: Failed to make directory {0} !\nPlease ensure that you have rights to make this directory, or that it isn\'t a file already.".format(options.output_folder)
+    makeDirCheck(options.output_folder)
+#    if not os.path.isdir(options.output_folder):
+#        try:
+#            os.mkdir(options.output_folder)
+#        except:
+#            print "\n\nERROR: Failed to make directory {0} !\nPlease ensure that you have rights to make this directory, or that it isn\'t a file already.".format(options.output_folder)
 
 os.chdir(options.output_folder)
 full_output_dir=os.getcwd() #For later reference...
@@ -125,8 +148,47 @@ if not os.path.isdir(os.path.join(starting_dir,"Kaiko/")):
 
 print "\n\n\n=================== Kaiko Execution is now starting. ===================\n\n\n"
 
+
+
+makeDirCheck(os.path.join(full_output_dir,"decode_output/"))
+#if not os.path.isdir(os.path.join(full_output_dir,"decode_output/")):
+#    try:
+#        os.mkdir(os.path.join(full_output_dir,"decode_output/"))
+#    except:
+#        print "\n\nERROR: Failed to make directory {0} !\nPlease ensure that you have rights to make this directory, or that it isn\'t a file already.".format(os.path.join(full_output_dir,"decode_output/"))
+
+
 tmp_cmd_str="{0}docker run --rm -v {1}:/app/model -v {2}:/app/mgf_input -v {2}/decode_output:/app/decode_output kaiko /bin/bash -c \"python ./src/kaiko_main.py --mgf_dir mgf_input/ --train_dir model/ --multi_decode --beam_search --beam_size {3} --topk {4}\"".format(sudo_str,os.path.join(starting_dir,"Kaiko/model/"),full_output_dir,options.kaiko_beam_size,options.kaiko_topk)
 print "About to execute command {0}".format(tmp_cmd_str)
 os.system(tmp_cmd_str)
 print "\n\n\n=================== Kaiko Execution has now finished ===================\n\n\n"
 
+print "\n\n\n=================== Starting file conversion to PEAKS style csv ===================\n\n\n"
+
+os.chdir(starting_dir)
+full_path_mzml=os.getcwd()
+###We're going to copy the mzML files over to the output dir of kaikonnect, which right now is the decode_output folder from Kaiko.
+for each_mzml in glob.glob(os.path.join(full_mzml_dir,"*.mzML")):
+    shutil.copy(each_mzml,os.path.join(os.path.join(full_output_dir,"decode_output/"),each_mzml.rsplit("/",1)[1]))
+
+### And we're going to make a folder for the converted TagGraph inputs to be dumped...
+makeDirCheck(os.path.join(full_output_dir,"decode_output/taggraph_input/"))
+
+#if not os.path.isdir(os.path.join(full_output_dir,"decode_output/taggraph_input/")):
+#    try:
+#        os.mkdir(os.path.join(full_output_dir,"decode_output/taggraph_input/"))
+#    except:
+#        print "\n\nERROR: Failed to make directory {0} !\nPlease ensure that you have rights to make this directory, or that it isn\'t a file already.".format(os.path.join(full_output_dir,"decode_output/taggraph_input/"))
+
+cmd_str_tmp="{0}docker run --rm -v {1}/decode_output/:/kaiko_output/ kaikonnect bash -c \"source activate python2 && python /kaikonnect/taggraph_interconnect.py\"".format(sudo_str, full_output_dir)
+print "\n\n\nPROGRESS: About to execute command : ",cmd_str_tmp,"\n\n\n"
+os.system(cmd_str_tmp)
+
+#Move those mzml copies into the taggraph input folder to allow taggraph to have a go at them....
+for each_mzml in glob.glob(os.path.join(os.path.join(full_mzml_dir,"decode_output/"),"*.mzML")):
+    os.rename(each_mzml,os.path.join(os.path.join(full_output_dir,"decode_output/taggraph_input/"),each_mzml.rsplit("/",1)[1]))
+
+
+
+#We'll also make a folder for the taggraph config...
+makeDirCheck(os.path.join(full_output_dir,"decode_output/taggraph_input/config/"))
