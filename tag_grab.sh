f5c6f1aaf64b16673c3244379a00e05a1f5ed84a
William Barshop <wbarshop@ucla.edu>
2020-04-14 10:38:43 -0700

    Update to TagGraph 1.8.1

--- tag_grab.sh
+++ tag_grab.sh
@@ -4,20 +4,21 @@
 sudo docker pull chambm/pwiz-skyline-i-agree-to-the-vendor-licenses && \
 chmod +x ${PWD}/build_docker.sh && \
 sudo ${PWD}/build_docker.sh && \
-wget https://sourceforge.net/projects/taggraph/files/Current%20Release/DockerContainer/linux/CentosDockerTG.1.7.1.tar.gz/download -O tg_docker.tar.gz && \
+wget https://sourceforge.net/projects/taggraph/files/CurrentRelease-TagGraph.1.8/DockerContainer/Centos7/CentosDockerTG.1.8.1.tar.gz/download -O tg_docker.tar.gz && \
 tar xzvf tg_docker.tar.gz && \
 rm tg_docker.tar.gz && \
 mkdir example && \
-mv CentosDockerTG.1.7.1/sampleInputFiles.tar.gz example/tg_examples.tar.gz && \
+mv CentosDockerTG.1.8.1/sampleInputFiles.1.8.docker.tar.gz example/tg_examples.tar.gz && \
 cd example/ && \
 mkdir mzml_files && \
 tar xzvf tg_examples.tar.gz && \
 rm tg_examples.tar.gz && \
-mv sampleInputFiles/mzML/* mzml_files/
-mv sampleInputFiles/FMIndices/human_uniprot_12092014_crap.fasta . && \
-rm -rf sampleInputFiles/ && \
+mv sampleInputFiles.1.8.docker/mzML/* mzml_files/
+mv sampleInputFiles.1.8.docker/FMIndices/human_uniprot_12092014_crap.fasta . && \
+rm -rf sampleInputFiles.1.8.docker/ && \
 cd ../ && \
-cd CentosDockerTG.1.7.1 && \
+cd CentosDockerTG.1.8.1 && \
+sed -i 's/pip install/pip install --upgrade pip \&\& pip install/' Dockerfile && \
 chmod +x ${PWD}/build.sh && \
 sudo ./build.sh && \
 cd .. && \
